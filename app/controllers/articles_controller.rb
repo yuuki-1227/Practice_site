0e210a19fb27ff23c1a2f7ae540a251158ef3f88
yuuki-1227 <uncle.ozin.ochiai@gmail.com>
2021-04-14 06:52:28 +0000

    詳細画面、新規投稿リダイレクト先設定

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,10 @@
 class ArticlesController < ApplicationController
   def index
-    @article = Article.all
+    @articles = Article.all
   end
 
   def show
+    @article = Article.find(params[:id])
   end
 
   def new
