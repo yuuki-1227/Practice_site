1e2787250e3e57b07f5ae612c18978d2227372d6
yuuki-1227 <uncle.ozin.ochiai@gmail.com>
2021-04-14 07:06:37 +0000

    [Add]編集機能、削除機能

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,6 +18,19 @@ class ArticlesController < ApplicationController
   end
 
   def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    article = Article.find(params[:id])
+    article.update(article_params)
+    redirect_to article_path(article)
+  end
+
+  def destroy
+    article = Article.find(params[:id])
+    article.destroy
+    redirect_to articles_path
   end
 
   private
