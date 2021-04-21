1dd4bfffd8aa690460c01da05604774ec9a875d8
yuuki-1227 <uncle.ozin.ochiai@gmail.com>
2021-04-21 07:10:57 +0000

    [Update]遷移画面の確認

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!,except: [:top]
+
   before_action :configure_permited_parameters, if: :devise_controller?
 
  def after_sign_in_path_for(resouce)
