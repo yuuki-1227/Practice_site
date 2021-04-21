37450ba45cb12fd6dc99f535c1afbf92cd53d424
yuuki-1227 <uncle.ozin.ochiai@gmail.com>
2021-04-21 06:06:59 +0000

    [Update]ログイン機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permited_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys:[:name])
+    devise_parameter_sanitizer.permit(:sign_in, keys:[:name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :profile_image])
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,14 @@ class UsersController < ApplicationController
     @article = @user.articles.all
   end
 
-  def create
+  def edit
+   @user = User.find(params[:id])
+  end
+
+  def update
     @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
   end
 
   private
